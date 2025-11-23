470b77d5853c4ee4ed556cc96e6a1b1c181572db
Ashish <ashish2024.ak@gmail.com>
2025-11-23 13:07:29 +0530

    Create system-variables.sh

---
+++ shell-scripting-for-devops/part1-shell-basics/system-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# System-defined variables give system environment information.
+
+echo "Home Directory: $HOME"   # User's home folder
+echo "Current Shell: $SHELL"   # Which shell is being used
+echo "Current User: $USER"     # Username
+echo "Working Directory: $PWD" # Present working directory
+echo "System PATH: $PATH"      # List of directories searched for commands
