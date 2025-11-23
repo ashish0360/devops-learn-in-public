e4206306daf3eed4d7205e16f0dcdc135ca6c93d
Ashish <ashish2024.ak@gmail.com>
2025-11-23 12:16:32 +0530

    Delete shell-scripting-for-devops/part1-shell-basics/#!/usr/bin directory

--- shell-scripting-for-devops/part1-shell-basics/#!/usr/bin/ps-grep-example.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Show running processes related to a keyword
-
-read -p "Enter process name: " process
-
-echo "Searching for process: $process"
-
-ps -ef | grep "$process" | grep -v grep
