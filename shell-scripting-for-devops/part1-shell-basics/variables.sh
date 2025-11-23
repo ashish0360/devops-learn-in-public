ac3bded1ca491c3068f9738215f5568c0606e166
Ashish <ashish2024.ak@gmail.com>
2025-11-23 12:13:26 +0530

    Create variables.sh

---
+++ shell-scripting-for-devops/part1-shell-basics/variables.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# User-defined variable
+name="Ashish"
+
+# System variables
+echo "User: $USER"
+echo "Home directory: $HOME"
+
+# Print custom variable
+echo "Hello, $name! Welcome to Shell Scripting."
